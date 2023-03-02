e91441b37d2746b10f4a10d3bb6dfac7d2e6ed4d
Kathryn Lenth <kathryn.lenth@gmail.com>
2023-03-02 11:50:04 -0700

    Added Type

---
+++ src/edu/westminstercollege/cmpt355/minijava/VoidType.java
@@ -0,0 +1,8 @@
+package edu.westminstercollege.cmpt355.minijava;
+
+public final class VoidType implements Type {
+
+    public static final VoidType Instance = new VoidType();
+
+    private VoidType() {}
+}
